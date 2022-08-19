30bcdb4d6adeb584d4eeeebeb9a4e9db6768f9e8
xcyyang <3071779003@qq.com>
2022-08-19 13:53:16 -0400

    update ctf_template

--- attack.ts
+++ attack.ts
@@ -17,25 +17,12 @@ async function main() {
   const exploit = await new Exploit__factory(signer).deploy();
   console.log("Exploit contract deployed to: ",exploit.address)
   
-  // Show balance
-  // const WBNB = IERC20__factory.connect(config['address']['wbnb'],signer);
-  // console.log("Attacker WBNB balance:", hre.ethers.utils.formatUnits(await WBNB.balanceOf(signer.address),await WBNB.decimals()))
-  
-
-  // Execute any function
-  const functionTx = await exploit.executeAnyFunction();
-  console.log("Execute some function: ",functionTx.hash)
-  const transactionResult = await functionTx.wait()
-  console.log(transactionResult)
-
   // Execute exploit contract
   const exploitTx = await exploit.attack({value: ethers.utils.parseEther("500")});
   console.log("Exploiting... transcation: ",exploitTx.hash)
-  await exploitTx.wait()
+  const exploirtReceipt = await exploitTx.wait()
+  console.log(exploirtReceipt)
 
-  // Display result
-  // console.log("Exploit complete.")
-  // console.log("Attacker WBNB balance:",hre.ethers.utils.formatUnits(await WBNB.balanceOf(signer.address),await WBNB.decimals()))
 }
 
 main().catch((error) => {
